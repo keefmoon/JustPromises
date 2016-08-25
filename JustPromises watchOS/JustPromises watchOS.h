72e2e31e7a50b944f681aa56dd0528cea3a9f343
Maciej Trybilo <maciej.trybilo@just-eat.com>
2016-08-25 17:50:07 +0100

    Added the watchOS target.

---
+++ JustPromises watchOS/JustPromises watchOS.h
@@ -0,0 +1,19 @@
+//
+//  JustPromises watchOS.h
+//  JustPromises watchOS
+//
+//  Created by Maciej Trybilo on 25/08/2016.
+//  Copyright © 2016 JUST EAT. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for JustPromises watchOS.
+FOUNDATION_EXPORT double JustPromises_watchOSVersionNumber;
+
+//! Project version string for JustPromises watchOS.
+FOUNDATION_EXPORT const unsigned char JustPromises_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustPromises_watchOS/PublicHeader.h>
+
+#import "JustPromises.h"
